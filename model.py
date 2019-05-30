8ce526725c006102f8ae2b034c97fea8c1dc47cf
sb-jang <namomo73@gmail.com>
2019-05-30 16:24:39 +0900

    extract img, post, hashtag embedding

--- model.py
+++ model.py
@@ -1,17 +1,7 @@
-from __future__ import print_function
-from __future__ import division
-
 import torch
 import torch.nn as nn
-import torch.optim as optim
-import numpy as np
-import torchvision
-from torchvision import datasets, models, transforms
-import matplotlib.pyplot as plt
-import time
-import os
-import copy
 import json
+from torchvision import models
 from allennlp.modules.elmo import Elmo, batch_to_ids
 
 data_dir = "./data/"
@@ -46,8 +36,8 @@ Output: feature vector of the image (512)
 def get_img_feature(img_tensor, model, layer):
 	img_tensor = img_tensor.unsqueeze(0)
 	# The 'avgpool' layer has an output size of 512
-	img_feature = torch.zeros(512)
-	img_feaure.to(device)
+	img_feature = torch.zeros(1, 512, 1, 1)
+	img_feature.to(device)
 
 	def copy_data(m, i, o):
 		img_feature.copy_(o.data)
@@ -56,7 +46,7 @@ def get_img_feature(img_tensor, model, layer):
 	model(img_tensor)
 	h.remove()
 
-	return img_feature
+	return img_feature.squeeze()
 
 # Post model part
 
@@ -75,81 +65,81 @@ def get_post_feature(post):
 
 # Hashtag model part
 
-class CharacterLevelCNN(nn.Module):
-	def __init__(self, args):
-		super(CharacterLevelCNN, self).__init__()
+# class CharacterLevelCNN(nn.Module):
+# 	def __init__(self, args):
+# 		super(CharacterLevelCNN, self).__init__()
 
-		# Load configuration such as model parameters
-		with open(args.config_path) as f:
-			self.config = json.load(f)
+# 		# Load configuration such as model parameters
+# 		with open(args.config_path) as f:
+# 			self.config = json.load(f)
 
-		# Creating conv layers
-		conv_layers = []
-		for i, conv_layer_parameter in enumerate(self.config['model_parameters'][args.size]['conv']):
-			if i == 0: # first conv layer
-				in_channels = args.number_of_characters + len(args.extra_characters)
-				out_channels = conv_layer_parameter[0]
-			else: # other conv layers
-				in_channels, out_channels = conv_layer_parameter[0], conv_layer_parameter[0]
+# 		# Creating conv layers
+# 		conv_layers = []
+# 		for i, conv_layer_parameter in enumerate(self.config['model_parameters'][args.size]['conv']):
+# 			if i == 0: # first conv layer
+# 				in_channels = args.number_of_characters + len(args.extra_characters)
+# 				out_channels = conv_layer_parameter[0]
+# 			else: # other conv layers
+# 				in_channels, out_channels = conv_layer_parameter[0], conv_layer_parameter[0]
 
-			if conv_layer_parameter[2] != -1: # layers with pooling
-				conv_layer = nn.Sequential(nn.Conv1d(in_channels, out_channels,
-					kernel_size=conv_layer_parameter[1], padding=0),
-				nn.ReLU(), nn.MaxPool1d(conv_layer_parameter[2]))
-			else: # layers without pooling
-				conv_layer = nn.Sequential(nn.Conv1d(in_channels, out_channels,
-					kernel_size=conv_layer_parameter[1], padding=0),
-				nn.ReLU())
-			conv_layers.append(conv_layer)
-		self.conv_layers = nn.ModuleList(conv_layers)
+# 			if conv_layer_parameter[2] != -1: # layers with pooling
+# 				conv_layer = nn.Sequential(nn.Conv1d(in_channels, out_channels,
+# 					kernel_size=conv_layer_parameter[1], padding=0),
+# 				nn.ReLU(), nn.MaxPool1d(conv_layer_parameter[2]))
+# 			else: # layers without pooling
+# 				conv_layer = nn.Sequential(nn.Conv1d(in_channels, out_channels,
+# 					kernel_size=conv_layer_parameter[1], padding=0),
+# 				nn.ReLU())
+# 			conv_layers.append(conv_layer)
+# 		self.conv_layers = nn.ModuleList(conv_layers)
 
-		input_shape = (args.batch_size, args_max_length,
-			args.number_of_characters + len(args.extra_characters))
-		dimension = self._get_conv_output(input_shape)
+# 		input_shape = (args.batch_size, args_max_length,
+# 			args.number_of_characters + len(args.extra_characters))
+# 		dimension = self._get_conv_output(input_shape)
 
-		print('dimension :', dimension)
+# 		print('dimension :', dimension)
 
-		# Creating fc layers
-		fc_layer_parameter = self.config['model_parameters'][args.size]['fc'][0]
-		fc_layers = nn.ModuleList([
-			nn.Sequential(
-				nn.Linear(dimension, fc_layer_parameter), nn.Dropout(0.5)),
-			nn.Sequential(
-				nn.Linear(fc_layer_parameter, fc_layer_parameter), nn.Dropout(0.5)),
-			nn.Linear(fc_layer_parameter, args_number_of_classes),])
-		self.fc_layers = fc_layers
+# 		# Creating fc layers
+# 		fc_layer_parameter = self.config['model_parameters'][args.size]['fc'][0]
+# 		fc_layers = nn.ModuleList([
+# 			nn.Sequential(
+# 				nn.Linear(dimension, fc_layer_parameter), nn.Dropout(0.5)),
+# 			nn.Sequential(
+# 				nn.Linear(fc_layer_parameter, fc_layer_parameter), nn.Dropout(0.5)),
+# 			nn.Linear(fc_layer_parameter, args_number_of_classes),])
+# 		self.fc_layers = fc_layers
 
-		if args.size == 'small':
-			self._create_weights(mean=0.0, std=0.05)
-		elif args.size == 'large':
-			self._create_weights(mean=0.0, std=0.02)
+# 		if args.size == 'small':
+# 			self._create_weights(mean=0.0, std=0.05)
+# 		elif args.size == 'large':
+# 			self._create_weights(mean=0.0, std=0.02)
 
-	# Initialize weights for conv and fc layers
-	def _create_weights(self, mean=0.0, std=0.05):
-		for module in self.modules():
-			if isinstance(module, nn.Conv1d) or isinstance(module, nn.Linear):
-				module.weight.data.normal_(mean, std)
+# 	# Initialize weights for conv and fc layers
+# 	def _create_weights(self, mean=0.0, std=0.05):
+# 		for module in self.modules():
+# 			if isinstance(module, nn.Conv1d) or isinstance(module, nn.Linear):
+# 				module.weight.data.normal_(mean, std)
 
-	def _get_conv_output(self, shape):
-		input = torch.rand(shape)
-		output= input.transpose(1, 2)
-		# forward pass through conv layers
-		for i in range(len(self.conv_layers)):
-			output = self.conv_layers[i](output)
+# 	def _get_conv_output(self, shape):
+# 		input = torch.rand(shape)
+# 		output= input.transpose(1, 2)
+# 		# forward pass through conv layers
+# 		for i in range(len(self.conv_layers)):
+# 			output = self.conv_layers[i](output)
 
-		output = output.view(output.size(0), -1)
-		n_size = output.size(1)
-		return n_size
+# 		output = output.view(output.size(0), -1)
+# 		n_size = output.size(1)
+# 		return n_size
 
-	def forward(self, input):
-		output = input.transpose(1, 2)
-		# forward pass through conv layers
-		for i in range(len(self.conv_layers)):
-			output = self.conv_layers[i](output)
+# 	def forward(self, input):
+# 		output = input.transpose(1, 2)
+# 		# forward pass through conv layers
+# 		for i in range(len(self.conv_layers)):
+# 			output = self.conv_layers[i](output)
 
-		output = output.view(output.size(0), -1)
+# 		output = output.view(output.size(0), -1)
 
-		# forward pass through fc layers
-		for i in range(len(self.fc_layers)):
-			output = self.fc_layers[i](output)
-		return output
+# 		# forward pass through fc layers
+# 		for i in range(len(self.fc_layers)):
+# 			output = self.fc_layers[i](output)
+# 		return output
